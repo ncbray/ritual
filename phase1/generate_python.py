d64b6a8678abff67e0c6a0983a97b60524d12845
Nick Bray <ncbray@gmail.com>
2018-01-21 08:35:38 -0800

    Factor TabbedWriter into base.

--- phase1/generate_python.py
+++ phase1/generate_python.py
@@ -1,54 +1,9 @@
 import cStringIO
 
 from base import TypeDispatcher, dispatch, python_types
+import base.io
 import interpreter
 import model
-
-class Block(object):
-    def __init__(self, out):
-        assert isinstance(out, TabbedWriter), out
-        self.out = out
-
-    def __enter__(self):
-        self.out.indent()
-
-    def __exit__(self, type, value, traceback):
-        self.out.dedent()
-
-
-class TabbedWriter(object):
-    def __init__(self, out):
-        self.out = out
-        self.indent_level = 0
-        self.indent_text = ''
-        self.buffer = ''
-
-    def indent(self):
-        #assert not self.dirty
-        self.indent_level += 1
-        self.indent_text = self.indent_level * '    '
-
-    def dedent(self):
-        # Can't check dirty because this may be an exception.
-        self.indent_level -= 1
-        self.indent_text = self.indent_level * '    '
-
-    def block(self):
-        return Block(self)
-
-    # TODO behaves badly when accumulating whitespace.
-    def write(self, text):
-        for l in text.splitlines(True):
-            self.buffer += l
-            eol = l.endswith('\n')
-            if not eol:
-                return
-            b = self.buffer.rstrip()
-            self.buffer = ''
-            if b:
-                self.out.write(self.indent_text)
-                self.out.write(b)
-            self.out.write('\n')
 
 alternate_tree_names = {
     'location': 'int',
@@ -245,7 +200,7 @@ import interpreter
 
 
 def generate_source(f):
-    out = TabbedWriter(cStringIO.StringIO())
+    out = base.io.TabbedWriter(cStringIO.StringIO())
     GeneratePython.visit(f, out)
     return out.out.getvalue()
 
