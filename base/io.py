e138340ec281995816a4f4024c4176839e89c695
Nick Bray <ncbray@gmail.com>
2018-01-30 21:53:56 -0800

    Fill empty blocks with TabbedWriter.

--- base/io.py
+++ base/io.py
@@ -1,12 +1,16 @@
 class Block(object):
-    def __init__(self, out):
+    def __init__(self, out, if_empty=None):
         assert isinstance(out, TabbedWriter), out
         self.out = out
+        self.if_empty = if_empty
 
     def __enter__(self):
         self.out.indent()
+        self.pos = self.out.pos
 
     def __exit__(self, type, value, traceback):
+        if self.pos == self.out.pos and self.if_empty:
+            self.out.write(self.if_empty)
         self.out.dedent()
 
 
@@ -16,6 +20,7 @@ class TabbedWriter(object):
         self.indent_level = 0
         self.indent_text = ''
         self.buffer = ''
+        self.pos = 0
 
     def indent(self):
         #assert not self.dirty
@@ -27,8 +32,8 @@ class TabbedWriter(object):
         self.indent_level -= 1
         self.indent_text = self.indent_level * '    '
 
-    def block(self):
-        return Block(self)
+    def block(self, if_empty=None):
+        return Block(self, if_empty)
 
     # TODO behaves badly when accumulating whitespace.
     def write(self, text):
@@ -42,4 +47,5 @@ class TabbedWriter(object):
             if b:
                 self.out.write(self.indent_text)
                 self.out.write(b)
+                self.pos += 1
             self.out.write('\n')
