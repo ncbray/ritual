af792ad685c23374ce194eb7d44e2bfe5d7b49ed
Nick Bray <ncbray@gmail.com>
2018-02-04 16:58:16 -0800

    Factor common grammar pieces into base.

--- lang/graph/parser.py
+++ lang/graph/parser.py
@@ -1,11 +1,9 @@
 def setup():
+    import lang.base
     import os.path
-    import phase1.parser
 
     src_file = os.path.join(os.path.dirname(__file__), 'graphlang.ritual')
-    with open(src_file) as f:
-        src = f.read()
-    phase1.parser.compile(src_file, src, globals())
+    lang.base.generate_parser(src_file, True, globals())
     return buildParser()
 
 
