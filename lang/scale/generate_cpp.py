3d3ad5f4a01db416fca995432dbec30f685cdf22
Nick Bray <ncbray@gmail.com>
2018-02-14 23:40:32 -0800

    Cleanup codegen.

--- lang/scale/generate_cpp.py
+++ lang/scale/generate_cpp.py
@@ -7,6 +7,7 @@ class Generator(object):
     def __init__(self, out):
         self.out = out
         self.names = {}
+        self.tmp_id = 0
     
     def get_name(self, obj):
         name = self.names.get(obj)
@@ -91,57 +92,74 @@ class GenerateExpr(object):
 
     @dispatch(model.IntLiteral)
     def visitIntLiteral(cls, node, used, gen):
-        return repr(node.value)
+        return repr(node.value), False
 
     @dispatch(model.DirectCall)
     def visitDirectCall(cls, node, used, gen):
-        args = [cls.visit(arg, True, gen) for arg in node.args]
+        args = [gen_arg(arg, gen) for arg in node.args]
         func_name = gen.get_name(node.f)
-        return '%s(%s)' % (func_name, ', '.join(args))
+        return '%s(%s)' % (func_name, ', '.join(args)), True
 
     @dispatch(model.TupleLiteral)
     def visitTupleLiteral(cls, node, used, gen):
-        args = [cls.visit(arg, used, gen) for arg in node.args]
         if used:
-            return 'std::make_tuple(' + ', '.join(args) + ')'
+            args = [gen_arg(arg, gen) for arg in node.args]
+            return 'std::make_tuple(' + ', '.join(args) + ')', False
+        else:
+            for arg in node.args:
+                gen_void(arg, gen)
+            return None, False
 
     @dispatch(model.GetLocal)
     def visitGetLocal(cls, node, used, gen):
-        return node.lcl.name
+        return node.lcl.name, False
 
     @dispatch(model.Sequence)
     def visitSeqeunce(cls, node, used, gen):
-        for i, child in enumerate(node.children):
-            text = cls.visit(child, False, gen)
-            if i < len(node.children) - 1:
-                gen.out.write(text).write(';\n')
-            else:
-                return text
+        for child in node.children[:-1]:
+            gen_void(child, gen)
+        return cls.visit(node.children[-1], used, gen)
 
     @dispatch(model.Assign)
     def visitAssign(cls, node, used, gen):
+        assert not used
         target = GenerateTarget.visit(node.target, gen)
-        value = cls.visit(node.value, True, gen)
-        return target + ' = ' + value
+        value, impure = cls.visit(node.value, True, gen)
+        gen.out.write(target + ' = ' + value + ';\n')
+        return None, True
 
     @dispatch(model.BinaryOp)
     def visitBinaryOp(cls, node, used, gen):
-        left = cls.visit(node.left, True, gen)
-        right = cls.visit(node.right, True, gen)
-        return '(%s %s %s)' % (left, node.op, right)
+        left = gen_arg(node.left, gen)
+        right = gen_arg(node.right, gen)
+        return '%s %s %s' % (left, node.op, right), True
 
     @dispatch(model.While)
     def visitWhile(cls, node, used, gen):
+        assert not used
         gen.out.write('while (true) {\n')
         with gen.out.block():
-            cond = cls.visit(node.cond, True, gen)
-            gen.out.write('if (!%s) break;\n' % cond)
-            body = cls.visit(node.body, False, gen)
-            gen.out.write(body + ';\n')
-        gen.out.write('}')
+            cond, _ = cls.visit(node.cond, True, gen)
+            gen.out.write('if (!(%s)) break;\n' % cond)
+            gen_void(node.body, gen)
+        gen.out.write('}\n')
+        return None, True
 
-        return '' # HACK
 
+def gen_arg(node, gen):
+    expr, impure = GenerateExpr.visit(node, True, gen)
+    if impure:
+        tmp = 'tmp_%d' % gen.tmp_id
+        gen.tmp_id += 1
+        gen.out.write('auto %s = %s;\n' % (tmp, expr))
+        return tmp
+    else:
+        return expr
+
+def gen_void(node, gen):
+    expr, impure = GenerateExpr.visit(node, False, gen)
+    if expr and impure:
+        gen.out.write(expr).write(';\n')
 
 
 class GenerateSource(object):
@@ -149,12 +167,14 @@ class GenerateSource(object):
 
     @dispatch(model.Function)
     def visitFunction(cls, node, gen):
+        gen.tmp_id = 0
         gen.out.write('\n')
         GenerateTypeRef.visit(node.t.rt, gen)
         gen.out.write(' ').write(gen.get_name(node)).write('(')
         gen_params(node.params, gen)
         gen.out.write(') {\n')
         with gen.out.block():
+            # Declare locals
             params = set([p.lcl for p in node.params])
             for lcl in node.locals:
                 if lcl in params:
@@ -162,8 +182,13 @@ class GenerateSource(object):
                 GenerateTypeRef.visit(lcl.t, gen)
                 gen.out.write(' ').write(lcl.name).write(';\n')
 
-            expr = GenerateExpr.visit(node.body, True, gen)
-            gen.out.write('return ' + expr + ';\n')
+            # Generate body
+            rt = node.t.rt
+            is_void = isinstance(rt, model.TupleType) and len(rt.children) == 0
+            if is_void:
+                gen_void(node.body, gen)
+            else:
+                gen.out.write('return ' + gen_arg(node.body, gen)).write(';\n')
         gen.out.write('}\n')
 
     @dispatch(model.ExternFunction)
